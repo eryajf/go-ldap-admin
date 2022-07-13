36d71e4ae7a9a565a98ff61af9afa6db13074cec
二丫讲梵 <eryajf@163.com>
2022-07-13 13:03:27 +0800

    fix: dockerfile (#77)

--- docker-start.sh
+++ docker-start.sh
@@ -2,7 +2,7 @@
 
 # 修改配置文件中的连接地址
 sed -i 's@localhost:389@openldap:389@g' /app/config.yml
-sed -i 's@host: localhost@host: mysql@g'  /aap/config.yml
+sed -i 's@host: localhost@host: mysql@g'  /app/config.yml
 
 # 等待依赖项初始化成功
 /app/wait
