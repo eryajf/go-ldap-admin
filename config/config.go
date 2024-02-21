5ea3968a77cd1f9ef9a4cbeec011998b00fa64b1
eryajf <eryajf@163.com>
2024-02-21 22:31:47 +0800

    feat: 修复parentid为0值的问题;补充启动时的初始化数据;用户密码加密类型可配置化

--- config/config.go
+++ config/config.go
@@ -140,16 +140,17 @@ type RateLimitConfig struct {
 }
 
 type LdapConfig struct {
-	Url                string `mapstructure:"url" json:"url"`
-	MaxConn            int    `mapstructure:"max-conn" json:"maxConn"`
-	BaseDN             string `mapstructure:"base-dn" json:"baseDN"`
-	AdminDN            string `mapstructure:"admin-dn" json:"adminDN"`
-	AdminPass          string `mapstructure:"admin-pass" json:"adminPass"`
-	UserDN             string `mapstructure:"user-dn" json:"userDN"`
-	UserInitPassword   string `mapstructure:"user-init-password" json:"userInitPassword"`
-	GroupNameModify    bool   `mapstructure:"group-name-modify" json:"groupNameModify"`
-	UserNameModify     bool   `mapstructure:"user-name-modify" json:"userNameModify"`
-	DefaultEmailSuffix string `mapstructure:"default-email-suffix" json:"defaultEmailSuffix"`
+	Url                        string `mapstructure:"url" json:"url"`
+	MaxConn                    int    `mapstructure:"max-conn" json:"maxConn"`
+	BaseDN                     string `mapstructure:"base-dn" json:"baseDN"`
+	AdminDN                    string `mapstructure:"admin-dn" json:"adminDN"`
+	AdminPass                  string `mapstructure:"admin-pass" json:"adminPass"`
+	UserDN                     string `mapstructure:"user-dn" json:"userDN"`
+	UserInitPassword           string `mapstructure:"user-init-password" json:"userInitPassword"`
+	GroupNameModify            bool   `mapstructure:"group-name-modify" json:"groupNameModify"`
+	UserNameModify             bool   `mapstructure:"user-name-modify" json:"userNameModify"`
+	DefaultEmailSuffix         string `mapstructure:"default-email-suffix" json:"defaultEmailSuffix"`
+	UserPasswordEncryptionType string `mapstructure:"user-password-encryption-type" json:"userPasswordEncryptionType"`
 }
 type EmailConfig struct {
 	Host string `mapstructure:"host" json:"host"`
